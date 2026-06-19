91e80a7ba5741fe25d5c9e4e423f2e68cb5bb965
Vladislav <steshenko.va@yandex.ru>
2026-06-19 21:30:11 +0300

    Connect form to Telegram worker

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   // URL вашего Cloudflare Worker (без слэша в конце)
   // Пример: 'https://internet-leads.your-name.workers.dev'
-  WORKER_URL: '',
+  WORKER_URL: 'https://internet-leads-worker.steshenko-va.workers.dev',
 
   // Рабочие часы (для сообщений на сайте)
   WORK_HOURS: '9:00–21:00',
